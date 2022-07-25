39adfabdf3135759bcbf996635bd4c8eb823eeda
Peter Urban <peter.urban@ugent.be>
2022-07-25 17:41:45 +0200

    formatting

--- src/pymodule/c_positionaloffsets.cpp
+++ src/pymodule/c_positionaloffsets.cpp
@@ -12,7 +12,7 @@
 // -- include pybind11 headers
 #include <pybind11/stl.h>
 
-namespace py        = pybind11;
+namespace py = pybind11;
 using namespace themachinethatgoesping::navigation;
 
 void init_c_positionaloffsets(py::module& m)
@@ -20,24 +20,25 @@ void init_c_positionaloffsets(py::module& m)
 
     py::class_<PositionalOffsets>(
         m, "PositionalOffsets", DOC(themachinethatgoesping, navigation, PositionalOffsets))
-        .def(
-            py::init<double, double, double, double, double, double>(),
-            DOC(themachinethatgoesping, navigation, PositionalOffsets, PositionalOffsets_2),
-            py::arg("x")     = 0,
-            py::arg("y")     = 0,
-            py::arg("z")     = 0,
-            py::arg("yaw")   = 0,
-            py::arg("pitch") = 0,
-            py::arg("roll")  = 0)
+        .def(py::init<double, double, double, double, double, double>(),
+             DOC(themachinethatgoesping, navigation, PositionalOffsets, PositionalOffsets_2),
+             py::arg("x")     = 0,
+             py::arg("y")     = 0,
+             py::arg("z")     = 0,
+             py::arg("yaw")   = 0,
+             py::arg("pitch") = 0,
+             py::arg("roll")  = 0)
+
         .def("__eq__",
              &PositionalOffsets::operator==,
-             DOC(themachinethatgoesping, navigation, PositionalOffsets, operator_eq), py::arg("rhs"))
-        .def_readwrite("x",&PositionalOffsets::x)
-        .def_readwrite("y",&PositionalOffsets::y)
-        .def_readwrite("z",&PositionalOffsets::z)
-        .def_readwrite("yaw",&PositionalOffsets::yaw)
-        .def_readwrite("pitch",&PositionalOffsets::pitch)
-        .def_readwrite("roll",&PositionalOffsets::roll)
+             DOC(themachinethatgoesping, navigation, PositionalOffsets, operator_eq),
+             py::arg("other"))
+        .def_readwrite("x", &PositionalOffsets::x)
+        .def_readwrite("y", &PositionalOffsets::y)
+        .def_readwrite("z", &PositionalOffsets::z)
+        .def_readwrite("yaw", &PositionalOffsets::yaw)
+        .def_readwrite("pitch", &PositionalOffsets::pitch)
+        .def_readwrite("roll", &PositionalOffsets::roll)
         // default copy functions
         __PYCLASS_DEFAULT_COPY__(PositionalOffsets)
         // default binary functions
