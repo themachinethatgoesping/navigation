2ed7d6875b287cc72c9ae7e9fb554af4e92d45f7
Peter Urban <peter.urban@ugent.be>
2025-10-17 23:13:35 +0200

    no nanomodule pch for windows for now

--- src/pch/navigation_navigation_nanopy.hpp
+++ src/pch/navigation_navigation_nanopy.hpp
@@ -3,16 +3,16 @@
 #pragma once
 
 #if !defined(__WIN32__) && !defined(_WIN32) && !defined(__WIN64__) && !defined(_WIN64)
-    #include <nanobind/nanobind.h>
-#endif
+#include <nanobind/nanobind.h>
 #include <nanobind/stl/string.h>
 #include <nanobind/stl/vector.h>
-#include <cmath>
+#endif
 #include <GeographicLib/Geocentric.hpp>
 #include <GeographicLib/Geodesic.hpp>
-#include <string>
 #include <GeographicLib/LocalCartesian.hpp>
-#include <math.h>
-#include <iostream>
-#include <string_view>
+#include <cmath>
 #include <exception>
+#include <iostream>
+#include <math.h>
+#include <string>
+#include <string_view>
